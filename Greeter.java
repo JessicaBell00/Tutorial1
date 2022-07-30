402227189856f95d6aea53e7dc28674ce50d72ac
Jessica Bell <je.bell.jb@gmail.com>
2022-07-30 12:40:49 +1200

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -17,6 +17,7 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
+	String arg1 = args[1];
     String language = args[0];
     String country = args[1];
     String name = args[2];
